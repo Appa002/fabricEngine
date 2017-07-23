9d88a71d73bd0fea871961a33a8e50ba16f8030e
batburger <a_modenbach@hotmail.com>
2017-07-23 09:30:49 +0200

    Leftovers from testing

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+/*
+	* Omni- and bidirectonal links
+	* Option to remove the sended data
+*/
+
+#include <behavior.hpp>
+#include <iostream>
+#include <types.hpp>
+#include <vector>
+#include <engine.hpp>
+#include <gameobject.hpp>
+
+
+using namespace fabric;
+using namespace std;
+
 int main() {
+	Engine::get()->eventLoop();
 	return 0;
 }
