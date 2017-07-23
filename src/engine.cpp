b017c392e94ef9acf5038843d05c305caf99182e
batburger <a_modenbach@hotmail.com>
2017-07-23 09:33:35 +0200

    Added basic event loop logic

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,9 +1,39 @@
 #include <engine.hpp>
+#include <iostream>
+#include <types.hpp>
+#include <gameobject.hpp>
+#include <behavior.hpp>
 
-int fabric::Engine::eventLoop() {
+using namespace std;
+using namespace fabric;
+
+int Engine::eventLoop() {
+
+	for (unsigned int i = 0; i < vLoadedGameObjects->size(); i++) {
+		vLoadedGameObjects->at(i)->update();
+	}
+
+	// To be debricated //
+	// Asks the user for an input,
+	// if it equals yes the exitRoutin is ran and the programm exits
+	// else the eventloop calls it self
+	char* input = new char[255];
+	cin >> input;
+	if (strcmp(input, "yes") == 0)
+		exitRoutin();
+	else 
+		eventLoop();
+
 	return 0;
 }
 
-int fabric::Engine::exitRoutin() {
+void Engine::loadGameObject(GameObject* gObj){
+	vLoadedGameObjects->push_back(gObj);
+}
+
+int Engine::exitRoutin() {
+
+	cout << "Good bye!" << endl;
+	Engine::get()->del();
 	return 0;
 }
