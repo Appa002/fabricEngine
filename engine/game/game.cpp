64a6d29b789db2c9b8152be3e69236f93d510b3a
batburger <eis.toad@gmail.com>
2017-08-01 19:15:58 +0200

    Removed old map logic

--- engine/game/game.cpp
+++ engine/game/game.cpp
@@ -4,18 +4,3 @@ Do NOT edit by hand unless you know what you're doing!
 The Engine wont load when this file has an error
 */
 #include <engine.hpp>
-
-
-//HEADER
-#include "scripts/header/Player.hpp" 
-
-
-void fabric::Engine::registerGameObject(){
-//ENGINE_REGISTER
-	game::Player* player = new game::Player();
-	fabric::Engine::vRegisteredGameObjects->push_back(player);
- 
-
-}
-
-
