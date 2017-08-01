e6e75ba24e27506babb57ed49d52dc42f7bf55ba
batburger <eis.toad@gmail.com>
2017-08-01 19:17:05 +0200

    Removed Engine Register

--- tools/scriptcreator/main.cpp
+++ tools/scriptcreator/main.cpp
@@ -18,110 +18,6 @@ vector<string> split(string str, char delimiter) {
 	return internal;
 }
 
-
-int addIncludeHeader(string name, char* path) {
-	ifstream iFile(path);
-	string str;
-	string fileContent = "";
-
-	if (!iFile)
-		return 1;
-
-	while (!iFile.eof())
-	{
-		getline(iFile, str);
-
-		str += "\n";
-		fileContent += str;
-
-		str = "";
-	}
-
-	iFile.close();
-
-	vector<string> befor = split(fileContent, '\n');
-	vector<string> after;
-
-	for (unsigned int i = 0; i < befor.size(); i++) {
-		after.push_back(befor.at(i));
-
-		if (befor.at(i) == "//HEADER") {
-			after.push_back("#include \"scripts/header/" + name + ".hpp\" ");
-		}
-	}
-
-	fileContent = "";
-
-	for (unsigned int i = 0; i < after.size(); i++) {
-		fileContent += after.at(i) + "\n";
-	}
-
-
-	//cout << fileContent << endl;
-
-
-	ofstream oFile(path);
-
-	oFile.write(fileContent.c_str(), fileContent.size());
-	oFile.close();
-	return 0;
-}
-
-
-int addEngineRegister(string name, char* path) {
-	ifstream iFile(path);
-	string str;
-	string fileContent = "";
-
-	if (!iFile)
-		return 1;
-
-	while (!iFile.eof())
-	{
-		getline(iFile, str);
-
-		str += "\n";
-		fileContent += str;
-
-		str = "";
-	}
-
-	iFile.close();
-
-	vector<string> befor = split(fileContent, '\n');
-	vector<string> after;
-
-	for (unsigned int i = 0; i < befor.size(); i++) {
-		after.push_back(befor.at(i));
-
-		if (befor.at(i) == "//ENGINE_REGISTER") {
-
-			string lowerName = name;
-			for (unsigned int i = 0; i < name.size(); i++)
-				lowerName[i] = tolower(name[i]);
-
-			after.push_back("	game::" + name + "* " + lowerName + " = new game::" + name + "();");
-			after.push_back("	fabric::Engine::vRegisteredGameObjects->push_back(" + lowerName + ");");
-			after.push_back(" ");
-		}
-	}
-
-	fileContent = "";
-
-	for (unsigned int i = 0; i < after.size(); i++) {
-		fileContent += after.at(i) + "\n";
-	}
-
-	//cout << fileContent;
-
-	ofstream oFile(path);
-
-	oFile.write(fileContent.c_str(), fileContent.size());
-	oFile.close();
-	return 0;
-}
-
-
 int addSourceFile(string name, string path) {
 	ofstream oFile(path + name + ".cpp");
 
@@ -155,7 +51,6 @@ int addSourceFile(string name, string path) {
 
 	return 0;
 }
-
 
 int addHeaderFile(string name, string path) {
 	ofstream oFile(path + name + ".hpp");
@@ -207,7 +102,6 @@ void waitForExit() {
 	
 }
 
-
 int main() {
 	string input;
 	int lhs;
@@ -228,28 +122,14 @@ int main() {
 	cin >> input;
 	cout << "Thank You";
 	cout << endl;
-
-	lhs = addIncludeHeader(input, "../../game/game.cpp");
-	if (lhs == 0)
-		cout << "... Include headers have been created in 'game.cpp'" << endl;
-	else
-		cout << "... Include header could not be written to 'game.cpp'" << endl;
-
 	
-	lhs = addEngineRegister(input, "../../game/game.cpp");
-	if (lhs == 0)
-		cout << "... Engine register have been created in 'game.cpp'" << endl;
-	else
-		cout << "... Engine register could not be written to 'game.cpp'" << endl;
-
-	
-	lhs = addSourceFile(input, "../../game/scripts/source/");
+	lhs = addSourceFile(input, "../../engine/game/scripts/source/");
 	if (lhs == 0)
 		cout << "... .cpp file has been created in 'scripts/source'" << endl;
 	else
 		cout << "... .cpp file could not be created in'scripts/source'" << endl;
 
-	lhs = addHeaderFile(input, "../../game/scripts/header/");
+	lhs = addHeaderFile(input, "../../engine/game/scripts/header/");
 	if (lhs == 0)
 		cout << "... .hpp file has been created in 'scripts/header'" << endl;
 	else
