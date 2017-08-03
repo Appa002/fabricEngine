025a64d0b6fdd3951bd944dca1d8a999213a66e7
batburger <eis.toad@gmail.com>
2017-08-03 20:35:02 +0200

    Added Correct Inilisation

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,23 +1,11 @@
-/*
-	* Omni- and bidirectonal links
-	* Option to remove the sended data
-*/
-
-#include <behavior.hpp>
-#include <iostream>
-#include <types.hpp>
-#include <vector>
 #include <engine.hpp>
-#include <gameobject.hpp>
 
+using namespace fabric;	
 
-using namespace fabric;
-using namespace std;
+int main(int argc, char* argv[]) {
 
-int main() {
-	Engine::get()->eventLoop();
-
-	Engine::del();
+	Engine::get()->startRoutin();
+	
 
 	return 0;
 }
