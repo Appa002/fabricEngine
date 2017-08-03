fec9eb3d59b1be1a0d1a9e94aec569ff248bf267
batburger <eis.toad@gmail.com>
2017-08-03 20:43:26 +0200

    Added Map loading logic

--- engine/src/map.cpp
+++ engine/src/map.cpp
@@ -1,6 +1,37 @@
-namespace fabric {
+#include <map.hpp>
+#include <iostream>
 
-	class Map {
+using namespace std;
+using namespace fabric;
 
-	};
+
+int Map::load(IMap* map) {
+	m_mMap = map;
+	std::cout << "Loading..." << std::endl;
+	map->load();
+
+	// Spawn all Behaviors for the gameobjs
+	for (unsigned int i = 0; i < Engine::get()->vLoadedGameObjects->size(); i++) {
+		Engine::get()->vLoadedGameObjects->at(i)->spawn();
+	}
+
+	std::cout << "Loaded ";
+	std::cout << Engine::get()->vLoadedGameObjects->size();
+	std::cout << " entitie(s)" << std::endl;
+
+	// Call Setup on just loaded gameobjects
+	for (unsigned int i = 0; i < Engine::get()->vLoadedGameObjects->size(); i++) {
+		Engine::get()->vLoadedGameObjects->at(i)->setup();
+	}
+
+
+	return 0;
+}
+
+
+int Map::unload() {
+	std::cout << "Unloading..." << std::endl;
+	m_mMap->unload();
+	std::cout << "Unloaded..." << std::endl;
+	return 0;
 }
