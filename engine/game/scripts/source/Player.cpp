c15d5cf0c87e61a50760525dfa2a7866bffd755e
batburger <eis.toad@gmail.com>
2017-08-03 20:37:43 +0200

    Added Hello, World in setup

--- engine/game/scripts/source/Player.cpp
+++ engine/game/scripts/source/Player.cpp
@@ -2,15 +2,20 @@
 #include <engine.hpp>
 #include <gameobject.hpp>
 #include <behavior.hpp>
+#include <iostream>
 
+
+using namespace std;
 using namespace fabric;
 
 namespace game{
 
 void Player::setup(){
 	// Use this for inital setup
+	cout << "Hello, World" << endl;
 	}
 void Player::update(){
 	// Use this for frame by frame logic
+
 	}
 }
