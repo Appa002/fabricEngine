1b085ec9b43f0cbb1659783b561f281810ee0055
batburger <eis.toad@gmail.com>
2017-08-03 20:39:32 +0200

    Inital Commit

--- engine/header/map.hpp
+++ engine/header/map.hpp
@@ -1,11 +1,37 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 
+#include <singleton.hpp>
+#include <vector>
+#include <string>
+#include <engine.hpp>
+#include <iostream>
+
+using namespace std;
+
+
 namespace fabric {
-
-	class Map {
-
+	
+	class IMap {
+	public:
+		virtual void load() {}
+		
+		void unload() {
+			for (unsigned int i = 0; i < Engine::get()->vLoadedGameObjects->size(); i++) {
+				delete Engine::get()->vLoadedGameObjects->at(i);
+			}
+		}
 	};
+
+	class Map: public Singleton<Map> {
+	public:
+		IMap* m_mMap;
+		int load(IMap* map);
+		int unload();
+	};
+
+
+
 
 }
 
