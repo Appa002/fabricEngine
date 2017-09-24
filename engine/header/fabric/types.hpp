72d05b45a843e121753e69a6d3dba5313e0dd609
batburger <eis.toad@gmail.com>
2017-09-24 20:14:25 +0200

    Added vector structs

--- engine/header/fabric/types.hpp
+++ engine/header/fabric/types.hpp
@@ -2,10 +2,39 @@
 #define TYPES_HPP
 
 namespace fabric {
-
-	enum FBenum
+	template<class T> struct Vector2D
 	{
-		FB_GAMEOBJECTS
+		T x;
+		T y;
+	};
+
+	template<class T> struct Vector3D
+	{
+		T x;
+		T y;
+		T z;
+	};
+
+	template<class T> struct Vector4D
+	{
+		T x;
+		T y;
+		T z;
+		T w;
+	};
+
+	typedef Vector2D<double> vec2;
+	typedef Vector3D<double> vec3;
+	typedef Vector4D<double> vec4;
+
+
+	enum fabric_state : unsigned long long {
+		FB_JUST_CREATED = 0,
+		FB_CONTEXT_CREATED = 1,
+		FB_STARTUP_COMPLETE = 2,
+		FB_LOOP_RUNNING = 4,
+		FB_ENDING = 8,
+		FB_ENDED = 16
 	};
 
 }
