fd78f714d83778c79c3cd7ad005367e3d55ffa62
batburger <eis.toad@gmail.com>
2017-09-29 17:10:24 +0200

    Removed Map testing code

--- main.cpp
+++ main.cpp
@@ -1,30 +1,13 @@
 #define UNIT_CATCH_UNIT_ERRORS
 #include <fabric/unit_framework.hpp>
 #include <fabric/engine.hpp>
-#include <fabric/tests.hpp>
-
-#include <fabric/map.hpp>
-#include <fabric/gameobjectHandler.hpp>
-#include <Windows.h>
 
 
 using namespace fabric;
 
 int main(int argc, char* argv[]){
 
-	
-
-	Map* map = new Map("./__game/badwater.lua");
-
-	map->open();
-	GameObjectHandler::get()->updateAll();
-
-
-	delete map;
-
-
 	fabric::Engine::get()->startRoutin();
-	
 	
 
 //	fabric::tests::registerTests();
