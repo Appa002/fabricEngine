910a661a2cdd07726300f84a7da92f3ae34ed8b3
batburger <eis.toad@gmail.com>
2017-09-29 17:13:04 +0200

    Position of gameobjects now can be controled via Attributes

--- engine/src/fabric/gameobject.cpp
+++ engine/src/fabric/gameobject.cpp
@@ -25,8 +25,8 @@ fabric::GameObject::GameObject() {
 
 	Attribute attr;
 	attr.content = new int(0);
-	attr.hash = typeid(int).hash_code();		// Initilize placeholder attribute so attributes.size() doesnt crash the code
-	attr.name = ""; 
+	attr.hash = typeid(int).hash_code();
+	attr.name = "0$"; 
 
 	GameObject::attributes.push_back(attr);
 }
@@ -57,6 +57,41 @@ fabric::Attribute fabric::GameObject::getAttribute(std::string name)
 
 int fabric::GameObject::render(){
 	VertexAttributeObject vao = GameObject::mesh.getVertexAttributeObject();
+
+	// transform attribute check
+	bool error = false; 
+	Attribute attrib = findAttribute<vec3>("transform", &error, 0);
+	vec3* content;
+
+	if (!error)
+		content = reinterpret_cast<vec3*>(attrib.content);
+	
+		
+	else {
+		content = new vec3();
+		content->x = 0;
+		content->y = 0;
+		content->z = 0;
+	}
+	VertexBufferObject vbo = VertexBufferObject(true, GL_ARRAY_BUFFER, GL_STATIC_DRAW);
+	
+	std::vector<GLdouble> myPoints;
+
+
+	// Super dumm solution from past me.
+	for (unsigned int i = 0; i < GameObject::mesh.getData().size(); i++) {
+		myPoints.push_back(content->x);
+		myPoints.push_back(content->y);
+		myPoints.push_back(content->z);
+	}
+
+	
+	vbo.make<GLdouble>(myPoints);
+
+	
+	vao.enableAttribArray(1);
+	vao.setVertexAtrrib(1, 3, vbo);
+
 	glBindVertexArray(vao.handle);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -66,4 +101,35 @@ int fabric::GameObject::render(){
 int fabric::GameObject::setMesh(Mesh _mesh) {
 	GameObject::mesh = _mesh;
 	return 0;
+}
+
+
+std::string fabric::GameObject::removeNameMangling(std::string name)
+{
+
+	bool mangled = false;
+	// 1. Check if name is mangled
+	for (size_t i = 0; i <= 4; i++) {
+		if (i < name.size()) {
+			if (name[i] == '$') {
+				mangled = true;
+				break;
+			}
+				
+		}
+		
+	}
+
+	if (!mangled)
+		return name;
+
+	// 2. Remove any mangeling
+	while (true) {
+		if (name[0] != '$')
+			name.erase(name.begin());
+		else {
+			name.erase(name.begin());
+			return name;
+		}
+	}
 }
