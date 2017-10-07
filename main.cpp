b661c2d56a388110eafa0769372481cb3ff369fc
batburger <eis.toad@gmail.com>
2017-10-07 10:28:51 +0200

    Removed UnitTestFramework load

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #define UNIT_CATCH_UNIT_ERRORS
 #include <fabric/unit_framework.hpp>
 #include <fabric/engine.hpp>
+#include <fabric/map.hpp>
+#include <fabric/gameobjectHandler.hpp>
 
+#include <fabric/engineObj.hpp>
 
 using namespace fabric;
 
-int main(int argc, char* argv[]){
 
+int main(int argc, char* argv[]){
 	fabric::Engine::get()->startRoutin();
 	
-
-//	fabric::tests::registerTests();
-//	fabric::UnitTestFramework::get()->doTests();
 	return 0;
 }
