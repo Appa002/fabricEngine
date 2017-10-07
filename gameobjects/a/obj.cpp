fcb2ee958dc1ee46aa07c8ea8ff1244ab162960b
batburger <eis.toad@gmail.com>
2017-10-07 10:35:27 +0200

    Added Transform Compontent

--- gameobjects/a/obj.cpp
+++ gameobjects/a/obj.cpp
@@ -2,6 +2,12 @@
 #include <string>
 #include <fabric/objectUtils.hpp>
 #include <fabric/types.hpp>
+#include <fabric/engine.hpp>
+#include <fabric/singleton.hpp>
+
+//class fabric::Singleton<fabric::Engine>;
+class fabric::Engine;
+
 
 EXPORT fabric::vec3 transform = fabric::vec3();
 
@@ -9,5 +15,5 @@ EXPORT void setup() {
 }
 
 EXPORT void update() {
-	std::cout << "Hello World" << std::endl;
+	
 }
