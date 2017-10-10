d8602dc69988940ab1b219b8dc20a4a069d12ddb
batburger <eis.toad@gmail.com>
2017-10-10 17:50:25 +0200

    Added mesh Attribute

--- __game/gameobject.lua
+++ __game/gameobject.lua
@@ -6,11 +6,17 @@ data = {
 	dll_0 = {
 		path = "./__game/gameobject.dll",
 
-		size = 1,
+		size = 2,
 		attribute_0 = {
 			name = "transform",
 			type = "vec3",
 			content = {x = 0, y = 0, z = 0}
+		},
+
+		attribute_1 = {
+			name = "mesh",
+			type = "string",
+			content = "./__game/cube.obj"
 		}
 	}
 }
