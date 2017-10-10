b0d3ff8bb98ed69b18f8cbe4fc4cda699d9b921b
batburger <eis.toad@gmail.com>
2017-10-10 17:48:09 +0200

    Added lookat and cross product functions

--- engine/header/fabric/transform.hpp
+++ engine/header/fabric/transform.hpp
@@ -10,6 +10,11 @@ namespace fabric {
 		vec3 rotation;
 		vec3 scale;
 	};
+
+	namespace transform {
+		void lookAt(vec3 camPos, vec3 targetPos, vec3 upVec);
+		vec3 cross(vec3 a, vec3 b);
+	}
 }
 
 #endif // !TRANSFORM_HPP
--- engine/src/fabric/transform.cpp
+++ engine/src/fabric/transform.cpp
@@ -1,1 +1,39 @@
 #include <fabric/transform.hpp>
+
+void fabric::transform::lookAt(vec3 camPos, vec3 targetPos, vec3 up)
+{
+	
+
+	vec3 distance;
+	distance.x = targetPos.x - camPos.x;
+	distance.y = targetPos.y - camPos.y;
+	distance.z = targetPos.z - camPos.z;
+
+	vec3 forward;
+
+	double m = sqrt(distance.x * distance.x + distance.y * distance.y + distance.z * distance.z);
+	forward.x = distance.x / m;
+	forward.y = distance.y / m;
+	forward.z = distance.z / m;
+
+	vec3 right = cross(forward, up);
+
+	std::vector<double> mat = {
+		right.x, right.y, right.z, 0,
+		up.x, up.y, up.z, 0,
+		-forward.x, -forward.y, -forward.z, 0,
+		0, 0, 0, 1
+	};
+	
+}
+
+fabric::vec3 fabric::transform::cross(vec3 a, vec3 b)
+{
+	vec3 out;
+	out.x = a.y * b.z - a.z * b.y;
+	out.y = a.z * b.x - a.x * b.z;
+	out.z = a.x * b.y - a.y * b.x;
+
+	return out;
+
+}
